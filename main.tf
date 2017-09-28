6ce991eadc18b668577764ce0b53d00a8577b76d
Erik Osterman <erik@cloudposse.com>
2017-09-28 14:37:00 -0700

    pass attributes to distribution

--- main.tf
+++ main.tf
@@ -27,12 +27,13 @@ module "logs" {
 }
 
 module "distribution_label" {
-  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.name}"
-  delimiter = "${var.delimiter}"
-  tags      = "${var.tags}"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  attributes = "${var.attributes}"
+  delimiter  = "${var.delimiter}"
+  tags       = "${var.tags}"
 }
 
 resource "aws_cloudfront_distribution" "default" {

98da54e268dbbd6ed3250943e8e765abd2059654
Erik Osterman <erik@cloudposse.com>
2017-10-17 10:03:42 -0700

    format

--- variables.tf
+++ variables.tf
@@ -127,8 +127,8 @@ variable "forward_query_string" {
 
 variable "forward_headers" {
   description = "Specifies the Headers, if any, that you want CloudFront to vary upon for this cache behavior. Specify `*` to include all headers."
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
 }
 
 variable "forward_cookies" {

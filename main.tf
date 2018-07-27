c06ddf9baa5b7814dbd3268b6b170a71717cf355
Rajinder Singh <rajindersingh@hotmail.com>
2018-07-27 10:29:38 -0500

    Added web_acl_id

--- main.tf
+++ main.tf
@@ -99,6 +99,8 @@ resource "aws_cloudfront_distribution" "default" {
   }
 
   cache_behavior = "${var.cache_behavior}"
+  
+  web_acl_id = "${var.web_acl_id}"
 
   restrictions {
     geo_restriction {

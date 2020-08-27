569056f306e9fa138df0719d12fd57dd53db2768
Frank Klaassen <frank@cloudright.nl>
2020-08-27 09:30:31 +0200

    add env to label

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "origin_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  delimiter  = var.delimiter
-  attributes = compact(concat(var.attributes, ["origin"]))
-  tags       = var.tags
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
+  namespace   = var.namespace
+  stage       = var.stage
+  name        = var.name
+  delimiter   = var.delimiter
+  attributes  = compact(concat(var.attributes, ["origin"]))
+  tags        = var.tags
+  environment = var.environment
 }
 
 resource "aws_cloudfront_origin_access_identity" "default" {

7c8c148e5f89520755c503eb3e2dcefb232ec95c
Frank Klaassen <frank@cloudright.nl>
2020-09-08 11:05:24 +0200

    handle outputs when disabled

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "cf_id" {
-  value       = aws_cloudfront_distribution.default[0].id
+  value       = try(aws_cloudfront_distribution.default[0].id, "")
   description = "ID of AWS CloudFront distribution"
 }
 
 output "cf_arn" {
-  value       = aws_cloudfront_distribution.default[0].arn
+  value       = try(aws_cloudfront_distribution.default[0].arn, "")
   description = "ID of AWS CloudFront distribution"
 }
 
@@ -14,27 +14,27 @@ output "cf_aliases" {
 }
 
 output "cf_status" {
-  value       = aws_cloudfront_distribution.default[0].status
+  value       = try(aws_cloudfront_distribution.default[0].status, "")
   description = "Current status of the distribution"
 }
 
 output "cf_domain_name" {
-  value       = aws_cloudfront_distribution.default[0].domain_name
+  value       = try(aws_cloudfront_distribution.default[0].domain_name, "")
   description = "Domain name corresponding to the distribution"
 }
 
 output "cf_etag" {
-  value       = aws_cloudfront_distribution.default[0].etag
+  value       = try(aws_cloudfront_distribution.default[0].etag, "")
   description = "Current version of the distribution's information"
 }
 
 output "cf_hosted_zone_id" {
-  value       = aws_cloudfront_distribution.default[0].hosted_zone_id
+  value       = try(aws_cloudfront_distribution.default[0].hosted_zone_id, "")
   description = "CloudFront Route 53 zone ID"
 }
 
 output "cf_origin_access_identity" {
-  value       = aws_cloudfront_origin_access_identity.default[0].cloudfront_access_identity_path
+  value       = try(aws_cloudfront_origin_access_identity.default[0].cloudfront_access_identity_path, "")
   description = "A shortcut to the full path for the origin access identity to use in CloudFront"
 }
 

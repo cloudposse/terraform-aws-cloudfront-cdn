dc5b7bda81a05d01653c7d5a0340a70df74511e6
Joe Niland <joe@originalmind.com.au>
2021-03-15 15:58:53 +1100

    Allow specifiying `force_destroy` for s3-log-storage submodule (#58)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,12 +2,18 @@ provider "aws" {
   region = var.region
 }
 
+resource "random_id" "id" {
+  byte_length = 8
+}
+
 module "cdn" {
   source = "../../"
 
   aliases            = var.aliases
   origin_domain_name = var.origin_domain_name
   parent_zone_name   = var.parent_zone_name
+  attributes         = [random_id.id.hex]
+  log_force_destroy  = true
 
   context = module.this.context
 }

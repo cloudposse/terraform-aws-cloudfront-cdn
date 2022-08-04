262c49cf11df0f883d5f1f1036ae462599c14784
PJ Bezilla <bezilla@gmail.com>
2022-08-04 10:53:47 -0700

    Testing local changes to figs-eng fork

--- main.tf
+++ main.tf
@@ -1,8 +1,53 @@
+terraform {
+  backend "s3" {
+  }
+}
+
+locals {
+  global_tags = {
+    Env = var.environment
+    Environment = var.environment
+    Stage       = var.environment
+    Name   = module.this.id
+    Namespace   = var.namespace
+    TopologyCategory  = "cdn"
+    TopologyMember    = "${var.unqualified_cdn_id}-cdn"
+    UnqualifiedCdnId  = local.UnqualifiedCdnId
+  }
+  s3_tags = {
+    Env = var.environment
+    Environment = var.environment
+    Stage       = var.environment
+    Namespace   = var.namespace
+    TopologyCategory  = "cdn"
+    TopologyMember    = "${var.unqualified_cdn_id}-cdn"
+    UnqualifiedCdnId  = local.UnqualifiedCdnId
+  }
+
+  UnqualifiedCdnId  = var.unqualified_cdn_id
+
+  ordered_cache = length(local.default_graphql_behavior) > 0 ? [ 
+     merge(local.default_graphql_behavior, {"path_pattern" = "/shop/graphql"}),
+     merge(local.default_graphql_behavior, {"path_pattern" = "/catalog/graphql"}),
+#     merge(local.paypal_ipn_behavior, {"path_pattern" = "/services/ping/paypal_ipn/*"})
+     ] : var.ordered_cache
+
+}
+
+module "base-label" {
+  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.24.1"
+  namespace = var.namespace
+  stage     = var.environment
+  name      = "${var.name}"
+  delimiter = "-"
+  tags      = local.global_tags
+}
+
 module "origin_label" {
   source  = "cloudposse/label/null"
   version = "0.25.0"
 
-  attributes = ["origin"]
+  attributes = ["ingress"]
 
   context = module.this.context
 }
@@ -10,12 +55,13 @@ module "origin_label" {
 resource "aws_cloudfront_origin_access_identity" "default" {
   count = module.this.enabled ? 1 : 0
 
-  comment = module.origin_label.id
+#  comment = module.origin_label.id
+  comment = module.this.id
 }
 
 module "logs" {
   source  = "cloudposse/s3-log-storage/aws"
-  version = "0.26.0"
+  version = "0.28.0"
 
   enabled                  = module.this.enabled && var.logging_enabled && length(var.log_bucket_fqdn) == 0
   attributes               = compact(concat(module.this.attributes, ["origin", "logs"]))
@@ -24,12 +70,16 @@ module "logs" {
   glacier_transition_days  = var.log_glacier_transition_days
   expiration_days          = var.log_expiration_days
   force_destroy            = var.log_force_destroy
+  allow_ssl_requests_only  = var.log_force_ssl
+  s3_object_ownership      = var.s3_object_ownership
 
   context = module.this.context
+  tags    = local.s3_tags
+
+  
 }
 
 resource "aws_cloudfront_distribution" "default" {
-  #bridgecrew:skip=BC_AWS_GENERAL_27:Skipping `Ensure CloudFront distribution has WAF enabled` because AWS WAF is indeed configurable and is managed via `var.web_acl_id`.
   count = module.this.enabled ? 1 : 0
 
   enabled             = var.distribution_enabled
@@ -116,34 +166,42 @@ resource "aws_cloudfront_distribution" "default" {
           origin_read_timeout      = lookup(origin.value.custom_origin_config, "origin_read_timeout", 60)
         }
       }
-      dynamic "s3_origin_config" {
-        for_each = lookup(origin.value, "s3_origin_config", null) == null ? [] : [true]
+      dynamic "origin_shield" {
+        for_each = var.origin_shield != null ? ["true"] : []
         content {
-          origin_access_identity = lookup(origin.value.s3_origin_config, "origin_access_identity", null)
+          enabled              = var.origin_shield.enabled
+          origin_shield_region = var.origin_shield.region
         }
       }
+#      dynamic "s3_origin_config" {
+#        for_each = lookup(origin.value, "s3_origin_config", null) == null ? [] : [true]
+#        content {
+#          origin_access_identity = lookup(origin.value.s3_origin_config, "origin_access_identity", null)
+#        }
+#      }
     }
   }
 
   viewer_certificate {
-    acm_certificate_arn            = var.acm_certificate_arn
-    ssl_support_method             = var.acm_certificate_arn == "" ? null : "sni-only"
+    acm_certificate_arn            = var.acm_certificate_arn != "" ? var.acm_certificate_arn : data.terraform_remote_state.cloudflare-dns-ssl.outputs.wildcard_certificate_arn
+    ssl_support_method             = var.acm_certificate_arn == "" ? var.ssl_support_method : "sni-only"
     minimum_protocol_version       = var.viewer_minimum_protocol_version
-    cloudfront_default_certificate = var.acm_certificate_arn == "" ? true : false
+    cloudfront_default_certificate = var.acm_certificate_arn == "" ? var.cloudfront_default_certificate : false
   }
 
   default_cache_behavior {
     allowed_methods            = var.allowed_methods
     cached_methods             = var.cached_methods
-    cache_policy_id            = var.cache_policy_id
-    origin_request_policy_id   = var.origin_request_policy_id
+    cache_policy_id            = var.cache_policy_id != null ? var.cache_policy_id : aws_cloudfront_cache_policy.default.id
+    origin_request_policy_id   = var.origin_request_policy_id != null ? var.origin_request_policy_id : aws_cloudfront_origin_request_policy.default.id
     target_origin_id           = module.this.id
     compress                   = var.compress
     response_headers_policy_id = var.response_headers_policy_id
 
     dynamic "forwarded_values" {
+      for_each = var.enable_forwarded_values == true ? [1] : []
       # If a cache policy or origin request policy is specified, we cannot include a `forwarded_values` block at all in the API request
-      for_each = try(coalesce(var.cache_policy_id), null) == null && try(coalesce(var.origin_request_policy_id), null) == null ? [true] : []
+#      for_each = try(coalesce(var.cache_policy_id), null) == null && try(coalesce(var.origin_request_policy_id), null) == null ? [true] : []
       content {
         headers = var.forward_headers
 
@@ -182,7 +240,7 @@ resource "aws_cloudfront_distribution" "default" {
   }
 
   dynamic "ordered_cache_behavior" {
-    for_each = var.ordered_cache
+    for_each = local.ordered_cache
 
     content {
       path_pattern = ordered_cache_behavior.value.path_pattern
@@ -232,7 +290,8 @@ resource "aws_cloudfront_distribution" "default" {
     }
   }
 
-  web_acl_id = var.web_acl_id
+#  web_acl_id = var.web_acl_id
+  web_acl_id = var.mode == "web" ? aws_wafv2_web_acl.web[0].arn : aws_wafv2_web_acl.api[0].arn
 
   restrictions {
     geo_restriction {
@@ -241,7 +300,55 @@ resource "aws_cloudfront_distribution" "default" {
     }
   }
 
-  tags = module.this.tags
+#  tags = module.this.tags
+  tags = local.global_tags
+}
+
+resource "aws_cloudfront_origin_request_policy" "default" {
+  name    = "${module.base-label.id}-default-request-policy"
+  cookies_config {
+    cookie_behavior = "none"
+  }
+  headers_config {
+    header_behavior = "none"
+  }
+  query_strings_config {
+    query_string_behavior = "none"
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "default" {
+  name    = "${module.base-label.id}-default-cache-policy"
+
+  min_ttl     = 0
+  default_ttl = 0
+  max_ttl     = 31536000
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "whitelist"
+      cookies {
+        items = var.forward_cookies_whitelisted_names
+      }
+    }
+
+    headers_config {
+      header_behavior = "whitelist"
+      headers {
+        items = var.forward_headers
+      }
+    }
+
+    query_strings_config {
+      query_string_behavior = "whitelist"
+      query_strings {
+        items = var.query_string_cache_keys
+      }
+    }
+
+    enable_accept_encoding_gzip   = true
+    enable_accept_encoding_brotli = true
+  }
 }
 
 module "dns" {
